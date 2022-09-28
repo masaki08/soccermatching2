c4f34c6261eab37349b093fa830e9032b7aeae20
masaki08 <108650138+masaki08@users.noreply.github.com>
2022-09-28 10:28:47 +0000

    finish

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,6 +2,8 @@ class CustomersController < ApplicationController
    before_action :authenticate_customer!, only: [:show]
 
   def show
+    #byebug
     @customer = Customer.find(params[:id])
+    #byebug
   end
 end
